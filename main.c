abca4d25c3b260666735f2d714b0c2782c8542c9
Celiel <celieldacio60@gmail.com>
2026-06-27 00:14:15 -0300

    Ajustado graficos criado controles

--- main.c
+++ main.c
@@ -1,67 +1,19 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
+#include <X11/keysym.h>
+#include <X11/XKBlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
-void drawFloor(Display *display, Window wi, GC gc, double vw, double vh)
+#define BROWN 0x895129
+#define BLUE 0x87ceeb
+#define GREEN 0x00ee00
+#define YELLOW 0xeeee00
+
+typedef struct
 {
-   
-    XSetForeground(display, gc, 0x895129);
-    XFillRectangle(display, wi, gc, 0, vh * 90, vw * 100, vh * 11);
-    XFlush(display);
-
-    XSetForeground(display, gc, 0x864826);
-    XFillRectangle(display, wi, gc, 0, vh * 90, vw * 100, vh * 1);
-    XFlush(display);
-}
-
-void drawBottomPipe(Display *display, Window wi, GC gc, int x, int y,  double vw, double vh)
-{
-
-
-
-    XSetForeground(display, gc, 0x00cc00);
-    XFillRectangle(display, wi, gc, x+2*vw, y+vh*8, vw*16, vh*60);
-    XFlush(display);
-
-    XSetForeground(display, gc, 0x00ee00);
-    XFillRectangle(display, wi, gc, x+3*vw, y+vh*8, vw*14, vh*60);
-    XFlush(display);
-
-    
-    // top part
-
-    XSetForeground(display, gc, 0x00cc00);
-    XFillRectangle(display, wi, gc, x, y, vw*20, vh*8);
-    XFlush(display);
-
-    XSetForeground(display, gc, 0x00ee00);
-    XFillRectangle(display, wi, gc, x+1*vw, y+0.5*vh, vw*20 - 2*vw, vh*7);
-    XFlush(display);
-}
-
-void drawTopPipe(Display *display, Window wi, GC gc, int x, int y,  double vw, double vh)
-{
-
-
-
-    XSetForeground(display, gc, 0x00cc00);
-    XFillRectangle(display, wi, gc, x+2*vw, y+vh*8, vw*16, vh*60);
-    XFlush(display);
-
-    XSetForeground(display, gc, 0x00ee00);
-    XFillRectangle(display, wi, gc, x+3*vw, y+vh*8, vw*14, vh*60);
-    XFlush(display);
-
-    
-    // top part
-
-    XSetForeground(display, gc, 0x00cc00);
-    XFillRectangle(display, wi, gc, x, y, vw*20, vh*8);
-    XFlush(display);
-
-    XSetForeground(display, gc, 0x00ee00);
-    XFillRectangle(display, wi, gc, x+1*vw, y+0.5*vh, vw*20 - 2*vw, vh*7);
-    XFlush(display);
-}
+    XRectangle bottom, top;
+} PipeGroup;
 
 int main()
 {
@@ -69,38 +21,176 @@ int main()
     Window parent = RootWindow(display, 0);
     double vw, vh;
     XWindowAttributes attrs;
-    Window wi = XCreateSimpleWindow(display, parent, 0, 0, 300, 500, 0, 0, 0x87ceeb);
+
+    PipeGroup pipeGroups[2];
+
+    XRectangle gap = {
+        .width = 50,
+        .height = 100,
+        .x = 0,
+        .y = 100};
+
+    XRectangle ground = {
+        .height = 50,
+        .width = 300,
+        .x = 0,
+        .y = 450};
+
+    pipeGroups[0].top = (XRectangle){
+        .width = 50,
+        .height = 0,
+        .y = 0,
+        .x = 0};
+
+    pipeGroups[0].bottom = (XRectangle){
+        .width = 50,
+        .height = 0,
+        .y = 0,
+        .x = 0};
+
+    pipeGroups[1].top = (XRectangle){
+        .width = 50,
+        .height = 0,
+        .y = 0,
+        .x = 100};
+
+    pipeGroups[1].bottom = (XRectangle){
+        .width = 50,
+        .height = 0,
+        .y = 0,
+        .x = 100};
+
+    XRectangle player = {
+        .width = 50,
+        .height = 50,
+        .y = 120,
+        .x = 120};
+    Bool pressed = False;
+    Bool suports_detectable_repeat;
+    XkbSetDetectableAutoRepeat(display, True, &suports_detectable_repeat);
+    Window wi = XCreateSimpleWindow(display, parent, 0, 0, 300, 500, 0, 0, BLUE);
+    XGetWindowAttributes(display, wi, &attrs);
 
     Atom wm_close = XInternAtom(display, "WM_DELETE_WINDOW", False);
 
     GC gc = XCreateGC(display, wi, 0, NULL);
+
     XStoreName(display, wi, "Flappy Bird");
 
-    XSelectInput(display, wi, ExposureMask | StructureNotifyMask  );
+    XSelectInput(display, wi, ExposureMask | StructureNotifyMask | KeyPressMask | KeyReleaseMask);
+
     XSetWMProtocols(display, wi, &wm_close, 1);
     XMapWindow(display, wi);
     XFlush(display);
 
     XEvent event;
+    XKeyEvent kevent;
+    int vy = 0;
 
+    pipeGroups[0].bottom.x = attrs.width;
+    pipeGroups[0].top.x = attrs.width;
+
+    pipeGroups[1].bottom.x = attrs.width / 2 - pipeGroups[1].bottom.width / 2;
+    pipeGroups[1].top.x = attrs.width / 2 - pipeGroups[1].top.width / 2;
     while (1)
     {
-        XNextEvent(display, &event);
-        if (event.type == ClientMessage && event.xclient.data.l[0] == wm_close)
+        if (XPending(display) > 0)
         {
-            break;
+            XNextEvent(display, &event);
+
+            if (event.type == ClientMessage && event.xclient.data.l[0] == wm_close)
+            {
+                break;
+            }
+
+            // if(event.type == ButtonPress){
+            //     printf("Jump\n");
+            //     KeySym key = XKeycodeToKeysym(display, event.xkey.keycode, 0);
+            //     if(key == XK_space || key == XK_Up || key == XK_w || key == XK_W){
+            //         printf("Jump\n");
+            //         pressed = True;
+            //     }
+            // }
+
+            char keys[32];
+
+            XQueryKeymap(display, keys);
+
+            int index = event.xkey.keycode / 8;
+            int mask = 1 << (event.xkey.keycode % 8);
+
+            if (event.type == KeyPress)
+            {
+
+
+                KeySym key = XKeycodeToKeysym(display, event.xkey.keycode, 0);
+
+                if ((key == XK_space || key == XK_Up || key == XK_w || key == XK_W))
+                {
+                    if(!pressed){
+                        vy = -10;
+
+                        pressed = True;
+                    }
+                }
+            }
+            if (event.type == KeyRelease)
+            {
+
+                KeySym key = XKeycodeToKeysym(display, event.xkey.keycode, 0);
+
+                if ((key == XK_space || key == XK_Up || key == XK_w || key == XK_W))
+                {
+                    if(!(keys[index] & mask))
+                        pressed = False;
+                }
+            }
         }
 
-        if (event.type == Expose || event.type == ConfigureNotify)
-        {
-            XGetWindowAttributes(display, wi, &attrs);
-            vw = (double)attrs.width / 100;
-            vh = (double)attrs.height / 100;
-            
-            drawBottomPipe(display, wi, gc, 0, 50*vh, vw, vh);
 
-            drawFloor(display, wi, gc, vw, vh);
+        // if (event.type == Expose || event.type == Button1)
+        // {
+        int i;
+        XClearWindow(display, wi);
+
+        // pipes
+        XSetForeground(display, gc, GREEN);
+
+        for (i = 0; i < 2; i++)
+        {
+            pipeGroups[i].top.x -= 1;
+            pipeGroups[i].bottom.x -= 1;
+
+            if (pipeGroups[i].top.x < pipeGroups[i].bottom.width * -1)
+            {
+                pipeGroups[i].bottom.x = attrs.width;
+                pipeGroups[i].top.x = attrs.width;
+            }
+
+            pipeGroups[i].top.height = gap.y;
+            pipeGroups[i].bottom.y = gap.y + gap.height;
+            pipeGroups[i].bottom.height = attrs.height - pipeGroups[i].bottom.y - ground.height;
+
+            XFillRectangle(display, wi, gc, pipeGroups[i].top.x, pipeGroups[i].top.y, pipeGroups[i].top.width, pipeGroups[i].top.height);
+            XFillRectangle(display, wi, gc, pipeGroups[i].bottom.x, pipeGroups[i].bottom.y, pipeGroups[i].bottom.width, pipeGroups[i].bottom.height);
         }
+        XFlush(display);
+
+        // ground
+        XSetForeground(display, gc, BROWN);
+        XFillRectangle(display, wi, gc, ground.x, ground.y, ground.width, ground.height);
+        XFlush(display);
+
+        // player
+        XSetForeground(display, gc, YELLOW);
+        XFillRectangle(display, wi, gc, player.x, player.y, player.width, player.height);
+        XFlush(display);
+
+        vy += 1;
+        player.y+=vy;
+        // }
+
+        usleep(30000);
     }
 
     XUnmapWindow(display, wi);
